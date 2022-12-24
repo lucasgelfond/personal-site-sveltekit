08b7acec96bcdc721e94a36fec6331167916fcc6
Lucas Gelfond <lucas_gelfond@brown.edu>
2022-12-24 14:27:59 -0800

    site content updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,117 @@
-<div class="container" />
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400&display=swap"
+		rel="stylesheet" />
+</svelte:head>
+
+<main>
+	<div class="container">
+		<p>
+			<br />
+			hi, i'm lucas! currently, i:
+			<br />
+		</p>
+		<ul>
+			<li>
+				live in <a href="https://www.youtube.com/watch?v=HNo5dCbKy0g"
+					>providence, ri</a
+				>, where i study computer science and english at brown. i also
+			</li>
+			<li>
+				co-lead reboot's <a href="https://reboothq.substack.com/p/fellowship-22"
+					>student fellowship</a>
+				(with my friend <a href="https://twitter.com/zhaovan8">ivan</a>!), a
+				cohort-based book club and writing workshop for 40 undergrads interested
+				in themes of technology, humanity, and power
+			</li>
+			<li>
+				write about the internet, art, and technology, most recently for <a
+					href="https://www.vice.com/en/contributor/lucas-gelfond">vice</a>
+				and
+				<a href="https://www.kernelmag.io/2/prison-phones-profits"
+					>kernel magazine</a
+				>. read more of my writing
+				<a href="/writing">here</a>, or subscribe to my substack,
+				<a href="https://www.zinemun.ch/p/yum?s=w">ZINE MUNCH</a>
+			</li>
+		</ul>
+		<p>in the past, i:</p>
+		<ul>
+			<li>
+				shipped a bunch of A/B tests and support for <a
+					href="https://twitter.com/gucaslelfond/status/1605338226314870784"
+					>LaTeX</a> as an engineering intern on Substack's growth product team
+			</li>
+			<li>
+				built tools for better civic participation as a software engineer in the
+				nyc department of city planning's
+				<a
+					href="https://www1.nyc.gov/site/planning/planning-level/urban-design/urban-design-overview.page">
+					urban design office</a>
+				(through
+				<a
+					href="https://blog.codingitforward.com/meet-the-2021-corps-members-new-york-ny-e67adb4ac14f">
+					coding it forward</a
+				>)
+			</li>
+			<li>
+				taught in brown's <a href="http://ethics.cs.brown.edu/"
+					>socially responsible computing</a>
+				initiative, most recently for our largest intro class (<a
+					href="https://cs.brown.edu/courses/cs015/">CS15</a
+				>), taught by professor andy van dam.
+			</li>
+			<li>
+				edited pieces for <a href="https://www.kernelmag.io/"
+					>kernel magazine</a>
+				and led editorial for the college hill independent's
+				<a href="http://theindy.org/section=Science%20&%20Technology/0"
+					>science and tech</a> section
+			</li>
+		</ul>
+		<p>
+			i'm most active on <a href="https://twitter.com/gucaslelfond">twitter</a>
+			and most reachable by
+			<a href="mailto:lucasgelfond@gmail.com">email</a> &mdash; please say hi!
+			you can also find me on
+			<a href="https://www.are.na/lucas-gelfond">are.na</a>,
+			<a href="https://github.com/lucasgelfond">github</a>,
+			<a href="https://www.goodreads.com/user/show/127129067-lucas-gelfond"
+				>goodreads</a
+			>,
+			<a href="https://www.instagram.com/gucaslelfond/">instagram</a>,
+			<a href="https://www.last.fm/user/lucasgelfond">last.fm,</a> and
+
+			<a href="https://letterboxd.com/lucasgelfond/">letterboxd</a>.
+		</p>
+		for professional and professional-adjacent things, find me on
+		<a href="https://linkedin.com/in/lucasgelfond">linkedin</a>
+		or read my
+		<a
+			href="https://drive.google.com/file/d/1OvSaSscmB_y61UQUpiiAd5RxxQYdXHB6/view?usp=sharing"
+			>resume
+		</a>
+	</div>
+</main>
 
 <style>
+	main {
+		text-align: left;
+		padding: 1em;
+		size: 16px;
+		line-height: 24px;
+		font-family: 'IBM Plex Mono', monospace;
+	}
 	.container {
-		display: flex;
+		display: block;
+		width: 80vw;
+	}
+	a {
+		color: #586990;
+	}
+	a:hover {
+		color: #041e42;
 	}
 </style>
--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -1,1 +1,271 @@
-<h1>here's my writin</h1>
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,200;0,300;0,400;0,500;0,700;1,200;1,400&display=swap"
+		rel="stylesheet" />
+</svelte:head>
+
+<main>
+	<p>
+		(favorites are in bold! this page was inspired by <a
+			href="https://www.kylechayka.com/">Kyle Chayka</a>
+		and
+		<a href="https://www.annawiener.com/writing">Anna Wiener's</a> websites)
+	</p>
+	<div class="container">
+		<div class="item">
+			<h3>writing:</h3>
+			<h4>2022</h4>
+			<ul>
+				<li>
+					On working in crypto after the crash (<a
+						href="https://www.vice.com/en/article/akepek/web3-crypto-jobs-in-a-bear-market-interviews"
+						>VICE</a
+					>)
+				</li>
+				<li>
+					On Aaron Swartz (<a
+						href="https://reboothq.substack.com/p/the-boys-own-internet"
+						>Reboot</a
+					>)
+				</li>
+				<li>
+					<strong>
+						Ameelio, and the limits of venture capital (<a
+							href="https://www.kernelmag.io/2/prison-phones-profits"
+							>Kernel Magazine</a
+						>)</strong>
+				</li>
+				<li>
+					On BeReal (<a href="https://dirt.substack.com/p/dirt-bereal">Dirt</a>)
+				</li>
+				<li>
+					<strong>
+						Gig labor and equity in NFTs (<a
+							href="https://www.vice.com/en/article/7kb37b/meet-the-fiverr-artists-selling-nft-crypto-blockchain-art"
+							>VICE</a
+						>)</strong>
+				</li>
+				<li>
+					Providence's emerging electronic music scene (<a
+						href="https://theindy.org/article/2694"
+						>The College Hill Independent</a
+					>)
+				</li>
+				<li>
+					On ZINE MUNCH (<a href="https://www.zinemun.ch/p/yum">ZINE MUNCH</a>)
+				</li>
+			</ul>
+			<br />
+			<h4>2021</h4>
+			<ul>
+				<li>
+					<strong
+						>On tech workers jumping into crypto (<a
+							href="https://www.vice.com/en/article/88g4m3/i-quit-my-job-to-do-crypto-full-time"
+							>VICE</a
+						>)</strong>
+				</li>
+				<li>
+					ConstitutionDAO, a crypto discord that almost bought the constitution
+					(<a
+						href="https://www.vice.com/en/article/xgdzbk/group-of-crypto-investors-on-discord-plans-to-buy-copy-of-us-constitution"
+						>Motherboard</a
+					>)
+				</li>
+				<li>
+					Reading David Graeber's <i>Debt</i>
+					(<a href="https://reboothq.substack.com/p/graeber">Reboot</a>)
+				</li>
+				<li>
+					<strong
+						>On commons-based peer production and how to fund culture (<a
+							href="https://reboothq.substack.com/p/cbpp">Reboot</a
+						>)</strong>
+				</li>
+				<li>
+					On TikTok, and the optimization of identity (<a
+						href="https://logicmag.io/kids/gay-in-a-tiktok-way/">Logic</a
+					>)
+				</li>
+				<li>
+					Reddit's WallStreetBets (<a href="https://theindy.org/article/2284"
+						>The College Hill Independent</a
+					>)
+				</li>
+				<li>
+					Providence's Shell terminal (<a
+						href="https://theindy.org/article/2314"
+						>The College Hill Independent</a
+					>)
+				</li>
+				<li>
+					<strong>
+						Burial's <i>Untrue</i> and Mark Fisher (<a
+							href="https://theindy.org/article/2325"
+							>The College Hill Independent</a
+						>)</strong>
+				</li>
+				<li>
+					<strong>
+						SHL0MS and NFT art absurdism (<a
+							href="https://theindy.org/article/2388"
+							>The College Hill Independent</a
+						>)</strong>
+				</li>
+				<br />
+			</ul>
+			<h4>2020</h4>
+			<ul>
+				<li>
+					On TikTok (<a href="https://theindy.org/article/2078"
+						>The College Hill Independent</a
+					>)
+				</li>
+				<li>
+					Lenny Cioe and Rhode Island Political Cooperative (<a
+						href="https://theindy.org/article/2091"
+						>The College Hill Independent</a
+					>)
+				</li>
+				<li>
+					Greek life and the tension of abolition (<a
+						href="https://theindy.org/article/2139"
+						>The College Hill Independent</a
+					>)
+				</li>
+
+				<li>
+					CA's Proposition 22 and the future of gig work (<a
+						href="https://theindy.org/article/2170"
+						>The College Hill Independent</a
+					>)
+				</li>
+			</ul>
+			<br />
+		</div>
+		<div class="item">
+			<h3>interviews:</h3>
+			<h4>2022</h4>
+			<ul>
+				<li>
+					<strong
+						>with Josh Glenn, on <i>Hermenaut</i> (<a
+							href="https://www.zinemun.ch/p/hermenaut">ZINE MUNCH</a
+						>)
+					</strong>
+				</li>
+				<li>
+					with Paul Soullelis, on counterpublics, 'activist downloading,' and
+					the risograph(<a href="https://www.zinemun.ch/p/soulellis"
+						>ZINE MUNCH</a
+					>)
+				</li>
+				<li>
+					<strong
+						>with @meetmeintranspecos, on two shell, shitposting, and one
+						direction (<a href="https://www.zinemun.ch/p/two-shell"
+							>ZINE MUNCH</a
+						>)</strong>
+				</li>
+				<li>
+					with Milo Miller, dangerous archives and mail networks (<a
+						href="https://www.zinemun.ch/p/qzap">ZINE MUNCH</a
+					>)
+				</li>
+				<li>
+					<strong
+						>with Annie Rauwerda, on wikipedia esoterica and rogue editing (<a
+							href="https://www.zinemun.ch/p/depthsofwikipedia">ZINE MUNCH</a
+						>)</strong>
+				</li>
+				<li>
+					with Josh MacPhee, re chomsky and the kinko's night shift (<a
+						href="https://www.zinemun.ch/p/interferencearchive">ZINE MUNCH</a
+					>)
+				</li>
+				<li>
+					<strong
+						>with Alexander Star, on academia's gossip rag (<a
+							href="https://www.zinemun.ch/p/lingua-franca">ZINE MUNCH</a
+						>)</strong>
+				</li>
+				<li>
+					with Jenna Freedman, on whether blogs can be zines(<a
+						href="https://www.zinemun.ch/p/blogs">ZINE MUNCH</a
+					>)
+				</li>
+				<li>
+					with Emily Liu and Jessica Dai, on Kernel Magazine (<a
+						href="https://www.zinemun.ch/p/kernel">ZINE MUNCH</a
+					>)
+				</li>
+			</ul>
+			<br />
+			<h4>2021</h4>
+			<ul>
+				<li>
+					with Lachlan Kermode, on Forensic Architecture and open source
+					intelligence (<a href="https://theindy.org/article/2492"
+						>The College Hill Independent</a
+					>)
+				</li>
+			</ul>
+			<br />
+		</div>
+	</div>
+</main>
+
+<style>
+	a {
+		text-decoration: none;
+		color: #ff0000;
+	}
+
+	main {
+		text-align: left;
+		padding: 1em;
+		size: 16px;
+		line-height: 24px;
+		font-family: 'IBM Plex Mono', monospace;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: row;
+		column-gap: 5vw;
+		margin-right: 10vw;
+	}
+	.item {
+		flex-basis: 50%;
+	}
+
+	li {
+		font-weight: 200;
+	}
+	p {
+		margin-top: 4px;
+		font-weight: 200;
+	}
+
+	h4 {
+		margin: 0px;
+		font-weight: 400;
+		font-size: 18px;
+	}
+	h3 {
+		font-weight: 500;
+	}
+
+	strong {
+		font-weight: 400;
+	}
+	a {
+		color: #000000;
+		text-decoration: underline;
+	}
+	a:hover {
+		color: #041e42;
+	}
+</style>
