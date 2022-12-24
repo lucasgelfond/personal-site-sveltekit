9d3abdc37b3584d67c576bf2e51c32aa22493a07
Lucas Gelfond <lucas_gelfond@brown.edu>
2022-12-24 11:42:03 -0800

    initial config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,33 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	ignorePatterns: ['*.cjs,', '*.config.js'],
+	extends: ['airbnb-base-typescript-prettier', 'prettier'],
+	plugins: ['svelte3', '@typescript-eslint', 'prettier'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte3/svelte3',
+		},
+	],
 	settings: {
-		'svelte3/typescript': () => require('typescript')
+		'svelte3/typescript': () => require('typescript'),
+	},
+	rules: {
+		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+		'import/no-mutable-exports': 0,
+		'no-labels': 0,
+		'no-restricted-syntax': 0,
+		'prettier/prettier': 'error',
 	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		project: './tsconfig.json',
 	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
-	}
+		node: true,
+	},
 };
---
+++ src/routes/writing/+page.svelte
@@ -0,0 +1,1 @@
+<h1>here's my writin</h1>
